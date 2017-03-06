681df10da28ff0d7260f112db54c4c9a6d883eba
James Canning <james@jamescanning.com>
2017-03-06 14:30:00 +0000

    Implement skeleton of purchasing ☠

--- src/swe/engine/Market.java
+++ src/swe/engine/Market.java
@@ -2,6 +2,9 @@ package swe.engine;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Market {
     private ArrayList<Company> companies;
@@ -35,13 +38,68 @@ public class Market {
     public HashMap<Portfolio, TradeSlip> getRequestedPortfolioTrades() {
         HashMap<Portfolio, TradeSlip> trades = new HashMap<>();
         for (Portfolio portfolio : this.portfolios) {
-            trades.put(portfolio, portfolio.getRequestedTrades());
+            TradeSlip tradeSlip = portfolio.getRequestedTrades();
+            if (tradeSlip != null) {
+                trades.put(portfolio, tradeSlip);
+            }
         }
 
         return trades;
     }
 
     public void commitBestCaseTrades(HashMap<Portfolio, TradeSlip> trades) {
+        // merge all requested trades in to buy & sell (with deltas)
+
+        HashMap<Company, HashMap<Portfolio, Integer>> tradesByCompany = new HashMap<>();
+
+        // for each company
+        for (Map.Entry<Portfolio, TradeSlip> tradeSlip : trades.entrySet()) {
+            for(Map.Entry<Company, Integer> trade : tradeSlip.getValue().entrySet()) {
+                tradesByCompany.getOrDefault(trade.getKey(), new HashMap<>()).put(tradeSlip.getKey(), trade.getValue());
+            }
+        }
+
+        for (Map.Entry<Company, HashMap<Portfolio, Integer>> companyTrades : tradesByCompany.entrySet()) {
+            this.commitTradeForCompany(companyTrades.getKey(), companyTrades.getValue());
+        }
+    }
+
+    private void commitTradeForCompany(Company company, HashMap<Portfolio, Integer> portfolioDeltas) {
+        System.out.println(company.getName());
+
+        // separate out our buyers and sellers
+        Stream<Map.Entry<Portfolio, Integer>> buyers = portfolioDeltas.entrySet().stream().filter(set -> set.getValue() > 0);
+        Stream<Map.Entry<Portfolio, Integer>> sellers = portfolioDeltas.entrySet().stream().filter(set -> set.getValue() < 0);
+
+        // reduce those lists to total sell and buy amount
+        int totalBuy = buyers.map(Map.Entry::getValue).reduce(0, Integer::sum);
+        int totalSell = sellers.map(Map.Entry::getValue).reduce(0, Integer::sum) * -1;
+
+
+        // if we have no buyers OR sellers abort
+        if (totalBuy <= 0 || totalSell <= 0) {
+            return;
+        }
+
+        if (totalBuy >= totalSell) {
+            // TODO: ensure rounding is correct
+            Map<Portfolio, Integer> possibleBuy = buyers.collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    e -> totalSell * (e.getValue() / totalBuy)
+            ));
+
+            // sell all
+            // buy possibleBuy
+        } else {
+            // TODO: ensure rounding is correct
+            Map<Portfolio, Integer> possibleSell = sellers.collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    e -> totalSell * (e.getValue() / totalBuy)
+            ));
+
+            // buy all
+            // sell possibleSell
+        }
 
     }
 }
